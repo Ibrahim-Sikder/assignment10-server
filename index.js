43999048f18904fbedb2f79023d36cf1eef9e05e
freelancer-ibrahim <ibrahimfr4450@gmail.com>
2022-10-27 17:47:45 +0600

    loaded-data-from-server

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const cors = require('cors');
+const port = process.env.PORT || 5000;
 
 
+app.use(cors());
 
-app.get('/', (req,res)=>{
-    res.send("Hello I am running")
+const categories = require('./data/categories.json');
+
+const programming = require('./data/programming.json');
+
+
+app.get('/programming-categories', (req, res) => {
+    res.send(categories)
 })
 
+app.get('/main', (req, res) => {
+    res.send(categories)
+})
 
+app.get('/programming/:id', (req, res) => {
+    const id = req.params.id;
+    const programmingLanguage = programming.find( prog => prog._id === id)
+    res.send(programmingLanguage)
+})
 
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if( id === '08'){
+        res.send(programming)
+    }
+    else{
+        const category_language = programming.filter( prog => prog.category_id === id )
+        res.send(category_language)
+    }
+})
 
-app.listen(port, ()=>{
-    console.log('English learning website running', port);
+app.get('/', (req, res) => {
+    res.send('server is running now')
+})
+
+app.listen(port, () => {
+    console.log('kaj kortese')
 })
